f1c9d2603189a0e178b844d1969024fb61f221cd
caioagralemos <caioagralemos@live.com>
2023-03-20 22:28:30 -0300

    agralemos

--- models/projetos.js
+++ models/projetos.js
@@ -12,8 +12,11 @@ projetos[0] = new Projeto(
     0,
     'Pousada Haya',
     [
-      'http://pousadahaya.com.br/assets/img/manguezal/001.JPG',
-      'http://pousadahaya.com.br/assets/img/haya/006.JPG'
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358120/0-PousadaHayaok/IMG_2363_lpophe.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358118/0-PousadaHayaok/IMG_2367_kfpjzu.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358118/0-PousadaHayaok/PHOTO-2020-10-22-09-23-01_b2sls6.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358118/0-PousadaHayaok/867eea9a-c1f2-4eba-8e8c-dcf5e1966495_mdtlvq.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358124/0-PousadaHayaok/21bc4493-e9bc-4a8b-a031-c20bcf711fc6_cigfyp.jpg'
     ]
   );
 
@@ -21,60 +24,75 @@ projetos[1] = new Projeto (
     1,
     'Pousada Zai',
     [
-        'https://cf.bstatic.com/xdata/images/hotel/max1280x900/423126000.jpg?k=5af0df439d5cb029000fe5a42981304d525e1bd5b13266532b2002a1b962b2e4&o=&hp=1'
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358104/1-PousadaZai%20ok/PHOTO-2023-02-08-13-00-36_4_q5uicn.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358117/1-PousadaZai%20ok/PHOTO-2023-02-08-13-00-36_jeesr4.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358116/1-PousadaZai%20ok/PHOTO-2023-02-08-13-00-36_2_dtyx31.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358105/1-PousadaZai%20ok/PHOTO-2023-02-08-13-00-36_3_gmhnoa.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679358104/1-PousadaZai%20ok/PHOTO-2023-02-08-13-00-36_5_tfmvpq.jpg'
     ]
 )
 
 projetos[2] = new Projeto (
     2,
-    'Ibis Styles Maceió Pajuçara',
+    'Ibis Styles Pajuçara',
     [
         'https://www.ahstatic.com/photos/b0h9_ho_00_p_1024x768.jpg'
     ]
 )
 projetos[3] = new Projeto (
     3,
-    'Residência EMG',
+    'Estúdio Bruna Bert',
     [
-        'https://i.imgur.com/Cb9pWPG.jpg'
+        'https://www.ahstatic.com/photos/b0h9_ho_00_p_1024x768.jpg'
+
     ]
 )
 
 projetos[4] = new Projeto(
     4,
-    'Pousada Haya',
+    'Casa AA',
     [
-      'http://pousadahaya.com.br/assets/img/manguezal/001.JPG',
-      'http://pousadahaya.com.br/assets/img/haya/006.JPG'
+      'https://i.imgur.com/o6FsiI9.jpg',
+      'https://i.imgur.com/Z9SzcIg.jpg',
+      'https://i.imgur.com/BixY5dt.jpg',
+      'https://i.imgur.com/0NfwCou.jpg'
     ]
   );
 
 projetos[5] = new Projeto (
     5,
-    'Pousada Zai',
+    'Condomínio Moa',
     [
-        'https://cf.bstatic.com/xdata/images/hotel/max1280x900/423126000.jpg?k=5af0df439d5cb029000fe5a42981304d525e1bd5b13266532b2002a1b962b2e4&o=&hp=1'
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355338/5-Condomi%CC%81nioMoa%20ok/PHOTO-2021-05-28-18-10-13_e3lfxy.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355338/5-Condomi%CC%81nioMoa%20ok/PHOTO-2021-05-29-11-41-57_lkgtxw.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355338/5-Condomi%CC%81nioMoa%20ok/PHOTO-2021-05-28-10-36-11_jsaho2.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355338/5-Condomi%CC%81nioMoa%20ok/PHOTO-2021-05-28-18-10-13_e3lfxy.jpg'
     ]
 )
 
 projetos[6] = new Projeto (
     6,
-    'Ibis Styles Maceió Pajuçara',
+    'Mahré Hotel',
     [
-        'https://www.ahstatic.com/photos/b0h9_ho_00_p_1024x768.jpg'
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679356951/6-MahreHotel%20ok/PHOTO-2022-03-03-18-30-18_evchqb.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679356951/6-MahreHotel%20ok/PHOTO-2022-03-03-18-33-32_qc7fai.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679356951/6-MahreHotel%20ok/PHOTO-2022-03-03-15-45-05_sev9k1.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679356951/6-MahreHotel%20ok/PHOTO-2022-03-03-18-30-18_evchqb.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679356951/6-MahreHotel%20ok/PHOTO-2022-03-03-15-41-47_sfnm5x.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679356951/6-MahreHotel%20ok/PHOTO-2022-03-03-15-43-43_ks3akw.jpg'
     ]
 )
 projetos[7] = new Projeto (
     7,
-    'Residência EMG',
+    'Casa EI',
     [
-        'https://i.imgur.com/Cb9pWPG.jpg'
+        'https://www.ahstatic.com/photos/b0h9_ho_00_p_1024x768.jpg'
     ]
 )
 
 projetos[8] = new Projeto(
     8,
-    'Pousada Haya',
+    'Apartamento FG',
     [
       'http://pousadahaya.com.br/assets/img/manguezal/001.JPG',
       'http://pousadahaya.com.br/assets/img/haya/006.JPG'
@@ -83,38 +101,52 @@ projetos[8] = new Projeto(
 
 projetos[9] = new Projeto (
     9,
-    'Pousada Zai',
+    'Projeto NR',
     [
-        'https://cf.bstatic.com/xdata/images/hotel/max1280x900/423126000.jpg?k=5af0df439d5cb029000fe5a42981304d525e1bd5b13266532b2002a1b962b2e4&o=&hp=1'
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355355/9-ProjetoNR%20ok/PHOTO-2020-09-03-20-30-37_qlxchc.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355355/9-ProjetoNR%20ok/PHOTO-2020-09-03-20-30-37_qlxchc.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355355/9-ProjetoNR%20ok/PHOTO-2020-09-03-20-30-39_z9nwel.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355355/9-ProjetoNR%20ok/PHOTO-2020-09-08-17-09-37_htu0nu.jpg'
     ]
 )
 
 projetos[10] = new Projeto (
     10,
-    'Ibis Styles Maceió Pajuçara',
+    'Casa RR',
     [
-        'https://www.ahstatic.com/photos/b0h9_ho_00_p_1024x768.jpg'
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355356/10-CasaRR%20ok/PHOTO-2022-07-13-12-25-51_pk53zu.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/10-CasaRR%20ok/PHOTO-2022-07-08-15-32-47_rirr6g.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679357255/10-CasaRR%20ok/PHOTO-2020-08-26-16-44-19_ri1jhw.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679357255/10-CasaRR%20ok/PHOTO-2020-08-26-16-44-19_ri1jhw.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/10-CasaRR%20ok/PHOTO-2022-07-08-15-32-47_rirr6g.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/10-CasaRR%20ok/PHOTO-2022-07-12-18-03-36_f2pkf8.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/10-CasaRR%20ok/PHOTO-2022-07-08-15-34-02_kcqxl8.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/10-CasaRR%20ok/PHOTO-2022-07-13-12-25-51_pk53zu.jpg'
     ]
 )
 projetos[11] = new Projeto (
     11,
-    'Residência EMG',
+    'Apartamento FN',
     [
         'https://i.imgur.com/Cb9pWPG.jpg'
     ]
 )
 projetos[12] = new Projeto(
     12,
-    'Pousada Haya',
+    'Casa AC',
     [
-      'http://pousadahaya.com.br/assets/img/manguezal/001.JPG',
-      'http://pousadahaya.com.br/assets/img/haya/006.JPG'
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355356/12-CasaAC%20ok/PHOTO-2022-04-01-17-30-07_rqvw8z.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355357/12-CasaAC%20ok/PHOTO-2022-04-01-17-30-09_uzwk9r.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/12-CasaAC%20ok/PHOTO-2022-04-01-17-30-08_pit1m9.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/12-CasaAC%20ok/PHOTO-2022-04-01-17-30-10_qznycc.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/12-CasaAC%20ok/PHOTO-2022-04-01-17-30-07_rqvw8z.jpg',
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355356/12-CasaAC%20ok/PHOTO-2022-04-01-17-30-08_2_ddyc3n.jpg'
     ]
   );
 
 projetos[13] = new Projeto (
     13,
-    'Pousada Zai',
+    'Condomínio Serena',
     [
         'https://cf.bstatic.com/xdata/images/hotel/max1280x900/423126000.jpg?k=5af0df439d5cb029000fe5a42981304d525e1bd5b13266532b2002a1b962b2e4&o=&hp=1'
     ]
@@ -122,16 +154,67 @@ projetos[13] = new Projeto (
 
 projetos[14] = new Projeto (
     14,
-    'Ibis Styles Maceió Pajuçara',
+    'Casa MW',
     [
-        'https://www.ahstatic.com/photos/b0h9_ho_00_p_1024x768.jpg'
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355357/14-CasaMWok/PHOTO-2021-09-06-15-29-28_zngch8.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355357/14-CasaMWok/PHOTO-2021-09-06-15-29-28_zngch8.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355357/14-CasaMWok/PHOTO-2021-09-10-19-39-45_hannvy.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355357/14-CasaMWok/PHOTO-2021-09-06-15-29-28_2_hx0duz.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355357/14-CasaMWok/PHOTO-2021-09-06-15-29-28_3_vor8od.jpg'
     ]
 )
 projetos[15] = new Projeto (
     15,
-    'Residência EMG',
+    'Guajirú',
     [
-        'https://i.imgur.com/Cb9pWPG.jpg'
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355357/15-Guajiru%20ok/PHOTO-2022-06-03-17-51-13_2_wwouc6.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355357/15-Guajiru%20ok/PHOTO-2022-06-03-17-51-13_2_wwouc6.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355357/15-Guajiru%20ok/PHOTO-2022-06-03-17-51-17_x8vdw9.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355357/15-Guajiru%20ok/PHOTO-2022-06-03-17-51-15_jja0xo.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355358/15-Guajiru%20ok/PHOTO-2022-06-03-17-51-15_2_to1gur.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355358/15-Guajiru%20ok/PHOTO-2022-06-03-17-51-13_hsbb0t.jpg'
+    ]
+)
+projetos[16] = new Projeto(
+    16,
+    'Casa JA',
+    [
+      'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355473/16-CasaJA%20ok2/PHOTO-2023-02-25-15-08-17_3_zsodvq.jpg',
+      'https://i.imgur.com/nzr2xO7.jpg', 
+      'https://i.imgur.com/VkReBIq.jpg',
+      'https://i.imgur.com/uItjRPP.jpg',
+      'https://i.imgur.com/D1xQsg2.jpg'
+    ]
+  );
+
+projetos[17] = new Projeto (
+    17,
+    'Apartamento MS',
+    [
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/ar_1:1,c_fill,g_auto,w_1000/v1679355358/17-ApartamentoMS%20ok/PHOTO-2022-10-03-20-52-23_kcpbop.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355358/17-ApartamentoMS%20ok/PHOTO-2022-10-03-20-52-22_e9v6sc.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355358/17-ApartamentoMS%20ok/PHOTO-2022-10-03-13-31-43_rvywq0.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355358/17-ApartamentoMS%20ok/PHOTO-2022-09-29-15-01-10_xxegol.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355360/17-ApartamentoMS%20ok/PHOTO-2022-10-03-12-21-30_fwtkdv.jpg'
+    ]
+)
+
+projetos[18] = new Projeto (
+    18,
+    'Residência C',
+    [
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355359/18-CasaCecio%20ok/PHOTO-2022-09-21-21-35-12_2_d9xkpt.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355359/18-CasaCecio%20ok/PHOTO-2022-09-21-21-35-13_znxqrl.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355359/18-CasaCecio%20ok/PHOTO-2022-09-21-21-35-12_isp6xx.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355359/18-CasaCecio%20ok/PHOTO-2022-09-21-21-35-12_2_d9xkpt.jpg',
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/v1679355359/18-CasaCecio%20ok/PHOTO-2022-09-21-21-35-13_2_uhpxhe.jpg'
+    ]
+)
+projetos[19] = new Projeto (
+    19,
+    'Tropi',
+    [
+        'https://res.cloudinary.com/dcluyyuwo/image/upload/w_1000,ar_1:1,c_fill,g_auto/v1679355366/19-CasaM%20ok/PHOTO-2023-01-16-18-59-21_fv1lwx.jpg',
     ]
 )
 
